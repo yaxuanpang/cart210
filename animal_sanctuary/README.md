1ff4afc1c463256727f398874397edec823f1d7e
yaxuanpang <yaxuanpang0331@gmail.com>
2026-03-21 06:16:47 -0400

    added 2 new animals and a start button
    I added 2 animals that the player can "unlock" to add some difficulty to the game and I added a start button at the beginning of the game to have a clear indicator that the game started.

--- animal_sanctuary/README.md
+++ animal_sanctuary/README.md
@@ -2,21 +2,16 @@
 
 Yaxuan Pang
 
-[View this project online]
+[View this project online](http://yaxuanpang.github.io/cart210/animal_sanctuary/)
 
 ## Description
 
 
 ## Screenshot(s)
 
-> ![Image of my game]
-
 ## Attribution
 
 > - This project uses [p5.js](https://p5js.org).
-> - This projects uses the code from the variables challenge (https://concordia.yuja.com/V/Video?v=1071104&node=5700521&a=117175823)
-> - This projects uses the code from the conditionals challenge (https://yaxuanpang.github.io/cart253/topics/conditionals-challenge/)
-> - This projects uses the code from (https://editor.p5js.org/TheCurlyPasta/sketches/HkbMtLNJV) for the snowflakes.
 
 ## License
 
--- animal_sanctuary/js/script.js
+++ animal_sanctuary/js/script.js
@@ -1,5 +1,3 @@
-"use strict";
-
 //variables
 let x = 450;
 let y = 0;
@@ -13,6 +11,8 @@ let bearCount = 0;
 let dogCount = 0;
 let tigerCount = 0;
 let rhinoCount = 0;
+let pandaCount = 0;
+let pigeonCount = 0;
 
 let bearAlive = true;
 let bearHitReady = true;
@@ -26,12 +26,38 @@ let tigerHitReady = true;
 let rhinoAlive = true;
 let rihnoHitReady = true;
 
+let pandaAlive = false;
+let pandaHitReady = true;
+
+let pigeonAlive = false;
+let pigeonHitReady = true;
+
+let extraAnimals = false;
+
+let startTime;
+let pandaTimer;
+let pigeonTimer;
+
 let bearColor1;
 let bearColor2;
+
 let dogColor1;
 let tigerColor1;
+
 let rhinoColor1
 let rhinoColor2;
+
+let pandaColor1;
+let pandaColor2;
+
+let pigeonColor1;
+let pigeonColor2;
+
+let showPanda = false;
+let showPigeon = false;
+
+let showGrid = false;
+let showStart = true;
 
 //speed of the animals and initial position
 // Bear
@@ -42,11 +68,16 @@ let dx = 600, dy = 450, dspeedX = -1, dspeedY = 0.8;
 let tx = 500, ty = 100, tspeedX = 2, tspeedY = -1;
 // Rhino
 let rx = 300, ry = 270, rspeedX = 1, rspeedY = 3;
+//Panda
+let px = 550, py = 300, pspeedX = 0.5, pspeedY = 1;
+//Pigeon
+let pgx = 100, pgy = 400, pgspeedX = 1.2, pgspeedY = -0.5;
 
 
 //setup and original colors of the animals
 function setup() {
     createCanvas(900, 600);
+    angleMode(DEGREES);
     bearColor1 = color(94, 56, 10);
     dogColor1 = color(247, 213, 158);
     tigerColor1 = color(240, 150, 41);
@@ -54,15 +85,24 @@ function setup() {
 
     bearColor2 = color(130, 80, 30); // bear details color
     rhinoColor2 = color(220); // rhino horn
+
+    pandaColor1 = color(255);
+    pandaColor2 = color(0);
+
+    pigeonColor1 = color(150, 155, 165); // Pigeon Grey
+    pigeonColor2 = color(100, 105, 115); // Darker grey for wings
+
 }
 
 
 function draw() {
     background(0);
 
+    checkOverlap();
+    drawMoreAnimals();
+
     //move the animals and check for overlaps
     moveAnimals();
-    checkOverlap();
 
     //if the animal is alive, draw the animal of the canvas
     if (bearAlive) {
@@ -77,14 +117,95 @@ function draw() {
     if (rhinoAlive) {
         drawRhino();
     }
-
+    if (showStart) {
+        drawStart();
+    }
     //draw the grid (green dot)
-    drawGrid();
+    if (showGrid) {
+        drawGrid();
+    }
+}
+
+//draws the start button
+function drawStart() {
+
+    // check if mouse is hovering over the circle
+    let d = dist(mouseX, mouseY, width / 2, height / 2);
+    let hovering = d < 160;
+
+    fill(0);
+    rect(0, 0, width, height);
+
+
+
+    fill(255);
+    circle(width / 2, height / 2, 320);
+
+    fill(112, 153, 186, 70);
+    circle(width / 2, height / 2, 300);
+
+
+
+    if (hovering) {
+        fill(2, 173, 2);
+    } else {
+        fill(0, 255, 0);
+    }
+    circle(width / 2, height / 2, 290);
+
+    if (hovering) {
+        fill(200, 200, 200);
+    } else {
+        fill(255);
+    }
+    textSize(45);
+    textStyle(BOLD);
+    text("START", width / 2, height / 2);
+    textAlign(CENTER, CENTER);
+}
+
+//draws the pigeon and panda
+function drawMoreAnimals() {
+    //calculates the time after the the game started in milliseconds
+    let currentTime = millis() - startTime;
+
+    // if the 4 original animals are alive for more than 10 seconds, pandaAlive is true
+    if (bearAlive && dogAlive && tigerAlive && rhinoAlive && currentTime > 10000) {
+        if (!showPanda) {
+            pandaAlive = true;
+            showPanda = true;
+        }
+
+        //if the panda is alive and 20 seconds has passed, pigeonAlive is true
+        if (pandaAlive && currentTime > 20000 && !showPigeon) {
+            pigeonAlive = true;
+            showPigeon = true;
+        }
+    }
+
+    //if pandaAlive is true and 15 seconds has passed since the beginning of the game, draw the panda
+    if (pandaAlive && currentTime > 15000) {
+        drawPanda();
+    }
+
+
+    //is the pigeon is alive, draw the pigeon
+    if (pigeonAlive) {
+        drawPigeon();
+    }
+
 }
 
 //function to draw the grid
 function drawGrid() {
     //moveGrid();
+
+    if (showStart) {
+        bearAlive = false;
+        dogAlive = false;
+        tigerAlive = false;
+        rhinoAlive = false;
+    }
 
     //the grid remains a poor image fro 2 seconds
     let squareGrid = millis() - poorTimer < 2000;
@@ -198,8 +319,44 @@ function checkOverlap() {
         }
     }
 
+    if (pandaAlive) {
+        // gets hit by the grid
+        if (x > px && x < px + 80 && y > py && y < py + 80 && !squareActive) {
+            if (pandaHitReady) {
+                pandaCount++; // hit count goes up
+                pandaHitReady = false; // only counts as one hit
+            }
+            pandaColor1 = color(245, 62, 37);
+            pandaColor2 = color(181, 44, 25);
+        }
+        else {
+            pandaHitReady = true;
+            isGlitched = false;
+            pandaColor1 = color(255);
+            pandaColor2 = color(0);
+        }
+    }
+
+    if (pigeonAlive) {
+        // gets hit by the grid
+        if (x > pgx && x < pgx + 80 && y > pgy && y < pgy + 80 && !squareActive) {
+            if (pigeonHitReady) {
+                pigeonCount++; // hit count goes up
+                pigeonHitReady = false; // only counts as one hit
+            }
+            pigeonColor1 = color(245, 62, 37);
+            pigeonColor2 = color(181, 44, 25);
+        }
+        else {
+            pigeonHitReady = true;
+            isGlitched = false;
+            pigeonColor1 = color(150, 155, 165);
+            pigeonColor2 = color(100, 105, 115);
+        }
+    }
+
     //overlap counter
-    // if the animal is hti 3 times, it "dies"
+    // if the animal is hit 3 times, it "dies"
     if (bearCount === 3) {
         bearAlive = false;
     }
@@ -212,8 +369,15 @@ function checkOverlap() {
     if (rhinoCount === 3) {
         rhinoAlive = false;
     }
-}
+    if (pandaCount === 3) {
+        pandaAlive = false;
 
+    }
+    if (pigeonCount === 3) {
+        pigeonAlive = false;
+    }
+
+}
 
 //move the animals
 function moveAnimals() {
@@ -241,11 +405,34 @@ function moveAnimals() {
     ry += rspeedY;
     if (rx < 0 || rx > width - 80) rspeedX *= -1;
     if (ry < 0 || ry > height - 80) rspeedY *= -1;
-}
 
+    //panda
+    px += pspeedX;
+    py += pspeedY;
+
+    if (px < 0 || px > width - 80) pspeedX *= -1;
+    if (py < 0 || py > height - 80) pspeedY *= -1;
+
+    //pigeon
+    pgx += pgspeedX;
+    pgy += pgspeedY;
+
+    if (pgx < 0 || pgx > width - 80) pgspeedX *= -1;
+    if (pgy < 0 || pgy > height - 80) pgspeedY *= -1;
+}
 
 // mouse press function
 function mousePressed() {
+    if (!showGrid) {
+        let d = dist(mouseX, mouseY, width / 2, height / 2);
+        if (d < 160) {  // 160 = radius of the 320 diameter circle
+            showGrid = true;
+            showStart = false;
+            startTime = millis();  // start the timer when game begins
+        }
+        return;
+    }
+
     let locked = (clickCount >= 2 && millis() - lockTimer < 10000);
 
     if (locked) return;
@@ -258,40 +445,41 @@ function mousePressed() {
     if (clickCount === 2) {
         lockTimer = millis();
     }
+
+
 }
 
-
-
+//draws the animals
 function drawBear() {
     fill(bearColor1);
     push();
     noStroke();
 
-    // 1. EARS (Outer and Inner)
+    // 1. ears
     fill(bearColor1); // Dark brown
     circle(bx + 15, by + 10, 28); // Left outer
     circle(bx + 65, by + 10, 28); // Right outer
 
 
-    // 2. MAIN BODY
+    //body
     fill(bearColor1);
-    rect(bx, by, 80, 80, 20); // Rounded "Head"
+    rect(bx, by, 80, 80, 20);
 
-    // 3. THE SNOUT AREA (Muzzle)
+    //snout
     fill(bearColor2); // Lighter brown muzzle
     ellipse(bx + 40, by + 58, 45, 35);
 
-    // Actual Nose
+    // nose
     fill(20);
     ellipse(bx + 40, by + 50, 15, 10);
 
-    // Mouth line
+    // mouth
     stroke(20);
     strokeWeight(2);
     line(bx + 40, by + 55, bx + 40, by + 65); // Vertical line under nose
     noStroke();
 
-    // 4. EYES & BROWS
+    // eyes
     fill(255, 50); // Small white highlights behind eyes
     circle(bx + 25, by + 30, 15);
     circle(bx + 55, by + 30, 15);
@@ -306,22 +494,21 @@ function drawDog() {
     noStroke();
     fill(dogColor1);
     rect(dx, dy, 80, 80, 15);
-    // Floppy Ears
+    // ears
     rect(dx - 10, dy + 10, 20, 40, 10);
     rect(dx + 70, dy + 10, 20, 40, 10);
-    // Eyes & Nose
+    // eyes and nose
     fill(0);
     circle(dx + 25, dy + 30, 8);
     circle(dx + 55, dy + 30, 8);
     ellipse(dx + 40, dy + 55, 15, 10);
 }
 
-
 function drawTiger() {
     noStroke();
     push();
 
-    // 1. EARS
+    // ears
     stroke(tigerColor1);
     strokeWeight(9);
     strokeJoin(ROUND);
@@ -330,17 +517,17 @@ function drawTiger() {
     triangle(tx + 40, ty + 20, tx + 60, ty - 15, tx + 76.25, ty + 20); // Right
 
     noStroke();
-    // 2. MAIN HEAD
+    // head
     fill(tigerColor1);
     rect(tx, ty, 80, 80, 15);
 
-    // 3. WHITE SNOUT & EYE PATCHES
+    // snout and eye details
     fill(255, 255, 255, 180);
-    ellipse(tx + 40, ty + 60, 45, 30); // Snout area
-    circle(tx + 25, ty + 28, 20); // Left eye patch
-    circle(tx + 55, ty + 28, 20); // Right eye patch
+    ellipse(tx + 40, ty + 60, 45, 30); // Snout
+    circle(tx + 25, ty + 28, 20); // Left eye
+    circle(tx + 55, ty + 28, 20); // Right eye
 
-    // 4. STRIPES (Side and Forehead)
+    // stripes
     stroke(0);
     strokeWeight(3);
     // Side stripes (Tapered look using triangles or lines)
@@ -353,18 +540,18 @@ function drawTiger() {
     line(tx + 32, ty + 12, tx + 48, ty + 12);
     line(tx + 40, ty, tx + 40, ty + 15);
 
-    // 5. FACE FEATURES
+    // face details
     noStroke();
     fill(0);
     circle(tx + 25, ty + 30, 8); // Pupils
     circle(tx + 55, ty + 30, 8);
 
-    // Nose
+    // nose
     fill(0);
     triangle(tx + 35, ty + 52, tx + 45, ty + 52, tx + 40, ty + 58);
 
-    // Whiskers
-    stroke(255, 150); // Faint white whiskers
+    // whiskers
+    stroke(255, 150);
     strokeWeight(1);
     line(tx + 25, ty + 60, tx + 10, ty + 65);
     line(tx + 25, ty + 63, tx + 10, ty + 70);
@@ -373,7 +560,6 @@ function drawTiger() {
 
     pop();
 }
-
 
 function drawRhino() {
     noStroke();
@@ -390,4 +576,95 @@ function drawRhino() {
     fill(0);
     circle(rx + 25, ry + 35, 6);
     circle(rx + 55, ry + 35, 6);
+}
+
+function drawPanda() {
+    fill(pandaColor2);
+    push();
+    noStroke();
+
+    // 1. ears
+    stroke(pandaColor1);
+    fill(pandaColor2); // Dark brown
+    circle(px + 15, py + 10, 28); // Left outer
+    circle(px + 65, py + 10, 28); // Right outer
+
+
+    //body
+    fill(pandaColor1);
+    rect(px, py, 80, 80, 20);
+
+    //snout
+    fill(pandaColor2); // Lighter brown muzzle
+    ellipse(px + 40, py + 58, 45, 35);
+
+    // nose
+    fill(pandaColor2);
+    ellipse(px + 40, py + 50, 15, 10);
+
+    // mouth
+    stroke(pandaColor1);
+    strokeWeight(2);
+    line(px + 40, py + 55, px + 40, py + 65); // Vertical line under nose
+    noStroke();
+
+    // eyes
+    fill(pandaColor2); // Small white highlights behind eyes
+    stroke(pandaColor1);
+    circle(px + 25, py + 30, 15);
+    circle(px + 55, py + 30, 15);
+
+    fill(0);
+    circle(px + 25, py + 32, 8); // Pupils
+    circle(px + 55, py + 32, 8);
+    pop();
+}
+
+function drawPigeon() {
+    push();
+    noStroke();
+
+    //body
+    fill(pigeonColor1);
+    rect(pgx, pgy, 80, 80, 25);
+
+    noStroke();
+
+    // purple neck
+    fill(120, 80, 150, 200);
+    rect(pgx, pgy + 60, 75, 15, 5);
+
+    //green neck
+    fill(40, 160, 100, 200);
+    rect(pgx + 5, pgy + 50, 70, 12, 5);
+
+    // wings
+    fill(pigeonColor2);
+    arc(pgx + 10, pgy + 60, 30, 40, 90, 270);
+    arc(pgx + 70, pgy + 60, 30, 40, 270, 90);
+
+    //beak
+    fill(80);
+    triangle(pgx + 35, pgy + 50, pgx + 45, pgy + 50, pgx + 40, pgy + 65);
+
+
+    fill(240);
+    ellipse(pgx + 40, pgy + 48, 14, 10);
+
+    //eyes
+    fill(255, 100, 0); // Orange
+    circle(pgx + 18, pgy + 35, 14);
+    circle(pgx + 62, pgy + 35, 14);
+
+    fill(0); // Large pupils
+    circle(pgx + 18, pgy + 35, 7);
+    circle(pgx + 62, pgy + 35, 7);
+
+    // glint in eyes
+    fill(255);
+    circle(pgx + 20, pgy + 33, 3);
+    circle(pgx + 64, pgy + 33, 3);
+
+    pop();
+
 }
